c79628bacdd42c0e5b28a0959104faad50db06f7
Paulmburu <paulmburu53@gmail.com>
2017-12-12 01:31:25 +0300

    C++ learning (C++ Primer)

---
+++ TheStrs.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+
+using namespace std;
+
+int main(){
+	char a[10] ={"Paul "};
+	char b[10]={" mburu"};
+	string name =strcat(a,b);
+	
+	cout<<name<<endl;
+	
+	}
